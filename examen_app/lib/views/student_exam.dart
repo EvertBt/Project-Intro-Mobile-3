8d607ed078de189396b2034d21e392ade2ed264c
Evert Bekaert <bekaertevert@gmail.com>
2022-04-12 20:17:31 +0200

    Add question overview + individual question view

--- examen_app/lib/views/student_exam.dart
+++ examen_app/lib/views/student_exam.dart
@@ -23,6 +23,7 @@ class _StudentExam extends State<StudentExam> {
   int questionCount = 0;
   String progressText = "Voortgang: 0/0";
   double progress = 0.0;
+  Question? selectedQuestion;
 
   @override
   void initState() {
@@ -120,6 +121,18 @@ class _StudentExam extends State<StudentExam> {
     }
   }
 
+  void openQuestion(Question question) {
+    state = ExamState.question;
+    selectedQuestion = question;
+    setState(() {});
+  }
+
+  void showOverview() {
+    state = ExamState.questionOverview;
+    updateProgress();
+    setState(() {});
+  }
+
   IconData getProgressIcon(Question question) {
     if (question.answer != "") {
       return Icons.check_circle;
@@ -134,7 +147,7 @@ class _StudentExam extends State<StudentExam> {
       case ExamState.questionOverview:
         return overview(context);
       case ExamState.question:
-        return question(context);
+        return question(context, question: selectedQuestion);
     }
   }
 
@@ -144,7 +157,9 @@ class _StudentExam extends State<StudentExam> {
       height: 75.0,
       margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
       child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            openQuestion(exam.questions![i]);
+          },
           style: ElevatedButton.styleFrom(
             primary: buttonColor,
             onPrimary: Colors.white,
@@ -200,11 +215,11 @@ class _StudentExam extends State<StudentExam> {
                     ),
                     TextButton(
                       onPressed: () => {
-                        Navigator.pop(context, 'Begrepen'),
+                        Navigator.pop(context, 'Begrepen, verlaat examen'),
                         //await update student in firestore,
                         Navigator.pushNamed(context, homeRoute)
                       },
-                      child: const Text('Begrepen'),
+                      child: const Text('Begrepen, verlaat examen'),
                       style: TextButton.styleFrom(
                         primary: buttonColor,
                         onSurface: Colors.white,
@@ -365,7 +380,7 @@ class _StudentExam extends State<StudentExam> {
                               ),
                               Container(
                                 child: const Icon(
-                                  Icons.auto_stories,
+                                  Icons.book,
                                   color: Colors.white,
                                   size: 35,
                                 ),
@@ -380,24 +395,294 @@ class _StudentExam extends State<StudentExam> {
         ));
   }
 
-  Widget question(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(child: Container()),
-            Text(
-              apptitle,
-              style: const TextStyle(fontSize: 30),
+  Widget question(BuildContext context, {Question? question}) {
+    return WillPopScope(
+        onWillPop: () async {
+          showOverview();
+          return false;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(child: Container()),
+                Text(
+                  apptitle,
+                  style: const TextStyle(fontSize: 30),
+                ),
+                Expanded(child: Container()),
+                Text(widget.student.studentNr)
+              ],
             ),
-            Expanded(child: Container()),
-            Text(widget.student.studentNr)
-          ],
-        ),
-        backgroundColor: primaryColor,
-      ),
-      body: Center(),
-    );
+            backgroundColor: primaryColor,
+          ),
+          body: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(
+                      top: 15.0, bottom: 15.0, left: 20.0, right: 10.0),
+                  width: 700.0,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(blurRadius: 5, color: Colors.grey)
+                      ],
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Column(
+                    children: [
+                      Container(
+                          height: 70,
+                          decoration: BoxDecoration(
+                              boxShadow: const [
+                                BoxShadow(
+                                    color: Colors.grey,
+                                    blurRadius: 5,
+                                    offset: Offset(0, 3))
+                              ],
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(25.0)),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(left: 50),
+                                child: Text(
+                                  "Vraag ${exam.questions!.indexOf(question!) + 1}",
+                                  style: const TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Expanded(child: Container()),
+                              Container(
+                                margin: const EdgeInsets.only(right: 20),
+                                child: Text(
+                                  progressText,
+                                  style: const TextStyle(fontSize: 20),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(right: 50),
+                                height: 40,
+                                width: 40,
+                                child: CircularProgressIndicator(
+                                  backgroundColor: Colors.grey,
+                                  color: buttonColor,
+                                  strokeWidth: 10,
+                                  value: progress,
+                                ),
+                              )
+                            ],
+                          )),
+                      Expanded(child: Container())
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(
+                      top: 15.0, bottom: 15.0, left: 10.0, right: 20.0),
+                  width: 700.0,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(blurRadius: 5, color: Colors.grey)
+                      ],
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Column(
+                    children: [
+                      Container(
+                          height: 70,
+                          decoration: BoxDecoration(
+                              boxShadow: const [
+                                BoxShadow(
+                                    color: Colors.grey,
+                                    blurRadius: 5,
+                                    offset: Offset(0, 3))
+                              ],
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(25.0)),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(left: 50),
+                                child: const Text(
+                                  "Antwoord",
+                                  style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Expanded(child: Container()),
+                              exam.questions!.indexOf(question) + 1 <
+                                      exam.questions!.length
+                                  ? Container(
+                                      width: 250.0,
+                                      height: 50.0,
+                                      margin: const EdgeInsets.only(right: 10),
+                                      child: ElevatedButton(
+                                          onPressed: () {
+                                            openQuestion(exam.questions![exam
+                                                    .questions!
+                                                    .indexOf(question) +
+                                                1]);
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                            primary: buttonColor,
+                                            onPrimary: Colors.white,
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(25)),
+                                            padding: const EdgeInsets.all(8.0),
+                                            elevation: 5,
+                                          ),
+                                          child: Row(
+                                            children: [
+                                              Container(
+                                                margin: const EdgeInsets.only(
+                                                    left: 15),
+                                                child: const Text(
+                                                  'Volgende vraag',
+                                                  style:
+                                                      TextStyle(fontSize: 25.0),
+                                                ),
+                                              ),
+                                              Expanded(child: Container()),
+                                              const Icon(
+                                                Icons.arrow_circle_right,
+                                                size: 35,
+                                              )
+                                            ],
+                                          )),
+                                    )
+                                  : Container(
+                                      width: 285.0,
+                                      height: 50.0,
+                                      margin: const EdgeInsets.only(right: 10),
+                                      child: ElevatedButton(
+                                          onPressed: () {
+                                            showOverview();
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                            primary: buttonColor,
+                                            onPrimary: Colors.white,
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(25)),
+                                            padding: const EdgeInsets.all(8.0),
+                                            elevation: 5,
+                                          ),
+                                          child: Row(
+                                            children: [
+                                              Container(
+                                                margin: const EdgeInsets.only(
+                                                    left: 15),
+                                                child: const Text(
+                                                  'Vragen overzicht',
+                                                  style:
+                                                      TextStyle(fontSize: 25.0),
+                                                ),
+                                              ),
+                                              Expanded(child: Container()),
+                                              Container(
+                                                  margin: const EdgeInsets.only(
+                                                      right: 15),
+                                                  child: const Icon(
+                                                    Icons.auto_stories,
+                                                    size: 35,
+                                                  ))
+                                            ],
+                                          )),
+                                    ),
+                            ],
+                          )),
+                      Expanded(child: Container())
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          bottomNavigationBar: Container(
+            height: 56,
+            color: primaryColor,
+            child: Row(
+              children: [
+                Container(
+                    width: 100.0,
+                    height: 56.0,
+                    padding: const EdgeInsets.only(left: 10.0),
+                    decoration: const BoxDecoration(
+                      color: primaryColor,
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 13.0),
+                      child: const Image(
+                        image: AssetImage('assets/logosmall.png'),
+                        filterQuality: FilterQuality.high,
+                      ),
+                    )),
+                Expanded(
+                  child: Container(),
+                ),
+                exam.questions!.indexOf(question) + 1 < exam.questions!.length
+                    ? Container(
+                        decoration: const BoxDecoration(
+                          color: primaryColor,
+                        ),
+                        height: 56.0,
+                        width: 245.0,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              bottomLeft: Radius.circular(30),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromARGB(255, 99, 7, 0),
+                                  blurRadius: 10,
+                                  offset: Offset(0, 5))
+                            ],
+                            color: buttonColor,
+                          ),
+                          child: TextButton(
+                              onPressed: () {
+                                showOverview();
+                              },
+                              style: TextButton.styleFrom(
+                                primary: Colors.white,
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(30),
+                                        bottomLeft: Radius.circular(30))),
+                              ),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.only(left: 15),
+                                    child: const Text(
+                                      "Vragen overzicht",
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                  ),
+                                  Container(
+                                    child: const Icon(
+                                      Icons.auto_stories,
+                                      color: Colors.white,
+                                      size: 35,
+                                    ),
+                                    margin: const EdgeInsets.only(left: 15),
+                                  )
+                                ],
+                              )),
+                        ))
+                    : Container()
+              ],
+            ),
+          ),
+        ));
   }
 }
