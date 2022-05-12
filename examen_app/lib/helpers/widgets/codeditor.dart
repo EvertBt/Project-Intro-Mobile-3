93bcb0e89c8e427dec1c3c1f41b81453df5e8775
Evert Bekaert <bekaertevert@gmail.com>
2022-05-12 19:47:17 +0200

    Fix code correction answer

--- examen_app/lib/helpers/widgets/codeditor.dart
+++ examen_app/lib/helpers/widgets/codeditor.dart
@@ -5,11 +5,16 @@ import 'package:examen_app/helpers/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 class CodeEditor extends StatefulWidget {
-  const CodeEditor({required this.question, required this.controller, Key? key})
-      : super(key: key);
+  const CodeEditor({
+    required this.question,
+    required this.controller,
+    this.isExam = true,
+    Key? key,
+  }) : super(key: key);
 
   final CodeCorrectionQuestion question;
   final TextEditingController controller;
+  final bool isExam;
 
   @override
   State<CodeEditor> createState() => _CodeEditor();
@@ -49,109 +54,111 @@ class _CodeEditor extends State<CodeEditor> {
           ),
           cursorColor: buttonColor,
         )),
-        Row(
-          children: [
-            CustomButton(
-              margin: const EdgeInsets.only(right: 5),
-              width: 50,
-              height: 50,
-              buttonColor: buttonColor,
-              onPressed: () {
-                enterText("  ");
-              },
-              icon: const Icon(
-                Icons.format_indent_increase_rounded,
-                size: 35,
-              ),
-            ),
-            CustomButton(
-              margin: const EdgeInsets.only(left: 5, right: 5),
-              width: 50,
-              height: 50,
-              buttonColor: buttonColor,
-              onPressed: () {
-                enterText("{}", offset: -1);
-              },
-              buttonText: " { }",
-              fontSize: 25,
-            ),
-            CustomButton(
-              margin: const EdgeInsets.only(left: 5, right: 5),
-              padding: const EdgeInsets.only(left: 15),
-              width: 50,
-              height: 50,
-              buttonColor: buttonColor,
-              onPressed: () {
-                enterText("[]", offset: -1);
-              },
-              buttonText: "[ ]",
-              fontSize: 25,
-            ),
-            CustomButton(
-              margin: const EdgeInsets.only(left: 5, right: 5),
-              width: 50,
-              height: 50,
-              buttonColor: buttonColor,
-              onPressed: () {
-                enterText("()", offset: -1);
-              },
-              buttonText: " ( )",
-              fontSize: 25,
-            ),
-            CustomButton(
-              margin: const EdgeInsets.only(left: 5, right: 5),
-              width: 50,
-              height: 50,
-              buttonColor: buttonColor,
-              onPressed: () {
-                enterText("<>", offset: -1);
-              },
-              icon: const Icon(
-                Icons.code_rounded,
-                size: 35,
-              ),
-            ),
-            CustomButton(
-              margin: const EdgeInsets.only(left: 5, right: 5),
-              width: 50,
-              height: 50,
-              buttonColor: buttonColor,
-              buttonText: " =",
-              fontSize: 30,
-              padding: const EdgeInsets.fromLTRB(9, 0, 0, 3),
-              onPressed: () {
-                enterText("=");
-              },
-            ),
-            CustomButton(
-              margin: const EdgeInsets.only(left: 5, right: 5),
-              width: 50,
-              height: 50,
-              buttonColor: buttonColor,
-              buttonText: "  ;",
-              fontSize: 30,
-              padding: const EdgeInsets.fromLTRB(7, 0, 0, 6),
-              onPressed: () {
-                enterText(";");
-              },
-            ),
-            Expanded(child: Container()),
-            CustomButton(
-              width: 210,
-              height: 50,
-              buttonColor: buttonColor,
-              buttonText: "Formatteer",
-              onPressed: () {
-                widget.controller.value = widget.controller.value
-                    .copyWith(text: formatCode(widget.controller.text));
-              },
-              icon: const Icon(
-                Icons.format_align_right,
-                size: 35,
-              ),
-            ),
-          ],
-        )
+        widget.isExam
+            ? Row(
+                children: [
+                  CustomButton(
+                    margin: const EdgeInsets.only(right: 5),
+                    width: 50,
+                    height: 50,
+                    buttonColor: buttonColor,
+                    onPressed: () {
+                      enterText("  ");
+                    },
+                    icon: const Icon(
+                      Icons.format_indent_increase_rounded,
+                      size: 35,
+                    ),
+                  ),
+                  CustomButton(
+                    margin: const EdgeInsets.only(left: 5, right: 5),
+                    width: 50,
+                    height: 50,
+                    buttonColor: buttonColor,
+                    onPressed: () {
+                      enterText("{}", offset: -1);
+                    },
+                    buttonText: " { }",
+                    fontSize: 25,
+                  ),
+                  CustomButton(
+                    margin: const EdgeInsets.only(left: 5, right: 5),
+                    padding: const EdgeInsets.only(left: 15),
+                    width: 50,
+                    height: 50,
+                    buttonColor: buttonColor,
+                    onPressed: () {
+                      enterText("[]", offset: -1);
+                    },
+                    buttonText: "[ ]",
+                    fontSize: 25,
+                  ),
+                  CustomButton(
+                    margin: const EdgeInsets.only(left: 5, right: 5),
+                    width: 50,
+                    height: 50,
+                    buttonColor: buttonColor,
+                    onPressed: () {
+                      enterText("()", offset: -1);
+                    },
+                    buttonText: " ( )",
+                    fontSize: 25,
+                  ),
+                  CustomButton(
+                    margin: const EdgeInsets.only(left: 5, right: 5),
+                    width: 50,
+                    height: 50,
+                    buttonColor: buttonColor,
+                    onPressed: () {
+                      enterText("<>", offset: -1);
+                    },
+                    icon: const Icon(
+                      Icons.code_rounded,
+                      size: 35,
+                    ),
+                  ),
+                  CustomButton(
+                    margin: const EdgeInsets.only(left: 5, right: 5),
+                    width: 50,
+                    height: 50,
+                    buttonColor: buttonColor,
+                    buttonText: " =",
+                    fontSize: 30,
+                    padding: const EdgeInsets.fromLTRB(9, 0, 0, 3),
+                    onPressed: () {
+                      enterText("=");
+                    },
+                  ),
+                  CustomButton(
+                    margin: const EdgeInsets.only(left: 5, right: 5),
+                    width: 50,
+                    height: 50,
+                    buttonColor: buttonColor,
+                    buttonText: "  ;",
+                    fontSize: 30,
+                    padding: const EdgeInsets.fromLTRB(7, 0, 0, 6),
+                    onPressed: () {
+                      enterText(";");
+                    },
+                  ),
+                  Expanded(child: Container()),
+                  CustomButton(
+                    width: 210,
+                    height: 50,
+                    buttonColor: buttonColor,
+                    buttonText: "Formatteer",
+                    onPressed: () {
+                      widget.controller.value = widget.controller.value
+                          .copyWith(text: formatCode(widget.controller.text));
+                    },
+                    icon: const Icon(
+                      Icons.format_align_right,
+                      size: 35,
+                    ),
+                  ),
+                ],
+              )
+            : Container()
       ],
     );
   }
